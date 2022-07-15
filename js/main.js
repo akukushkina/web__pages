dc3723510769968b7d0b85aa6c2f1419939d9309
akyukyushkina <akyukyushkina@gmail.com>
2022-07-15 15:00:16 +0300

    add reviews

--- js/main.js
+++ js/main.js
@@ -28,3 +28,45 @@ load.addEventListener("click", () => {
 });
 
 
+// reviews slider
+
+const container = document.querySelector(".reviews__slider");
+const reviews = document.querySelectorAll(".review");
+const prev = document.querySelector(".rewiews__prev");
+const next = document.querySelector(".rewiews__next");
+
+let idx = 0;
+
+let interval = setInterval (run, 3000);
+
+function run(){
+    idx++
+    changeImage()
+}
+
+function changeImage() {
+    if(idx > reviews.length - 1){
+        idx = 0;
+    } else if( idx < 0){
+        idx = reviews.length - 1;
+    }
+
+    container.style.transform = `translateX(${-idx * 400}px)`;
+}
+
+function resetInterval(){
+    clearInterval(interval);
+interval = setInterval(run, 3000);
+}
+
+next.addEventListener('click', () => {
+    idx++;
+    changeImage();
+    resetInterval();
+})
+
+prev.addEventListener('click', () => {
+    idx--;
+    changeImage();
+    resetInterval();
+})
