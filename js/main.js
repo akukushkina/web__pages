d650941aa146b5b2887f31b23e59d7aec00eb2ee
akyukyushkina <akyukyushkina@gmail.com>
2022-07-15 19:21:42 +0300

    add contacts and summary

--- js/main.js
+++ js/main.js
@@ -2,18 +2,18 @@
 
 const video = document.querySelector(".about__video");
 const close = document.querySelector(".close");
-const play = document.querySelector(".video")
+const play = document.querySelector(".video");
 
 video.addEventListener("click", () => {
-    play.classList.toggle("active");
-    video.pause();
-    video.curentTime = 0;
-})
+  play.classList.toggle("active");
+  video.pause();
+  video.curentTime = 0;
+});
 
 close.addEventListener("click", () => {
-    play.classList.remove("active");
-})
-
+  play.classList.remove("active");
+  
+});
 
 // portfolio examples
 
@@ -22,11 +22,10 @@ const load = document.querySelector(".portfolio__btn");
 let index = 3;
 
 load.addEventListener("click", () => {
-    examples.forEach((example) => {
+  examples.forEach((example) => {
     example.classList.remove("hidden");
+  });
 });
-});
-
 
 // reviews slider
 
@@ -37,36 +36,51 @@ const next = document.querySelector(".rewiews__next");
 
 let idx = 0;
 
-let interval = setInterval (run, 3000);
+let interval = setInterval(run, 3000);
 
-function run(){
-    idx++
-    changeImage()
+function run() {
+  idx++;
+  changeImage();
 }
 
 function changeImage() {
-    if(idx > reviews.length - 1){
-        idx = 0;
-    } else if( idx < 0){
-        idx = reviews.length - 1;
-    }
+  if (idx > reviews.length - 1) {
+    idx = 0;
+  } else if (idx < 0) {
+    idx = reviews.length - 1;
+  }
 
-    container.style.transform = `translateX(${-idx * 395}px)`;
+  container.style.transform = `translateX(${-idx * 395}px)`;
 }
 
-function resetInterval(){
-    clearInterval(interval);
-interval = setInterval(run, 3000);
+function resetInterval() {
+  clearInterval(interval);
+  interval = setInterval(run, 3000);
 }
 
-next.addEventListener('click', () => {
-    idx++;
-    changeImage();
-    resetInterval();
-})
+next.addEventListener("click", () => {
+  idx++;
+  changeImage();
+  resetInterval();
+});
 
-prev.addEventListener('click', () => {
-    idx--;
-    changeImage();
-    resetInterval();
-})
+prev.addEventListener("click", () => {
+  idx--;
+  changeImage();
+  resetInterval();
+});
+
+// contacts form.
+
+const contactsBtn = document.querySelector(".contacts__btn");
+const contactsForm = document.querySelector(".form");
+const formClose = document.querySelector(".form__close")
+
+contactsBtn.addEventListener("click", () => {
+    contactsForm.classList.add('show');
+  });
+
+  formClose.addEventListener("click", () => {
+    contactsForm.classList.remove("show");
+  });
+
